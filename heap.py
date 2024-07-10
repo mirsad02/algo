b9e503cd6a244ffda12e3eece3f4b4182ee04646
Mirsad Avdic <40442379+mirsad02@users.noreply.github.com>
2024-07-10 11:42:05 +0200

    Update heap.py

--- heap.py
+++ heap.py
@@ -1,1 +1,22 @@
+def pq_parent(n):
+    if n == 1:
+        return 1
+    else:
+        return n/2
+
+def pq_young_child(n):
+    return 2 * n
+
+
+def bubble_up(q, p):
+    
+
+def pq_init(q):
+    q['n'] = 0
+
+def make_heap(q, s, n):
+    i = 0
+    pq_init(q)
+    while i < n:
+        pq_insert(q, s[i])
 
