69247945290b378ebd213f79d6bde38b1dfb40ed
Mirsad Avdic <40442379+mirsad02@users.noreply.github.com>
2024-07-12 13:37:28 +0200

    Update heap.py

--- heap.py
+++ heap.py
@@ -16,10 +16,16 @@ def bubble_up(q, p):
         #pq_swap(q, p, pq_parent(p))
         #bubble_up(q, pq_parent(p))
         
-def pq_insert(q, )
+def pq_insert(q, x):
+    if q['n'] > 99999999999:
+        return -1
+    else:    
+        q['n'] = q['n'] + 1
+        q['q'].insert(q['n'], x)
+        bubble_up(q, q['n'])
         
 def pq_init(q):
-    q['n'] = 0
+    q['n'] = -1
 
 def make_heap(q, s, n):
     i = 0
@@ -29,7 +35,10 @@ def make_heap(q, s, n):
         pq_insert(q, s[i])
         i = i + 1
 
-priority_queue = {}
-heap = []
+priority_queue = {
+    'n': 0,
+    'q': []
+}
+heap = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
     
 make_heap(priority_queue, heap, 20)
