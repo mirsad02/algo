67c611777417e3e364275de0a5f60cef0b4a9385
Mirsad Avdic <40442379+mirsad02@users.noreply.github.com>
2024-07-12 11:55:01 +0200

    Update heap.py

--- heap.py
+++ heap.py
@@ -1,6 +1,7 @@
+# slow heap creation
 def pq_parent(n):
     if n == 1:
-        return 1
+        return -1
     else:
         return n/2
 
@@ -9,14 +10,26 @@ def pq_young_child(n):
 
 
 def bubble_up(q, p):
-    
-
+    if pq_parent(p) == -1:
+        return;
+    #if q['q'][pq_parent(p)] > q['q']q[p]:
+        #pq_swap(q, p, pq_parent(p))
+        #bubble_up(q, pq_parent(p))
+        
+def pq_insert(q, )
+        
 def pq_init(q):
     q['n'] = 0
 
 def make_heap(q, s, n):
     i = 0
     pq_init(q)
+    print(q)
     while i < n:
         pq_insert(q, s[i])
+        i = i + 1
 
+priority_queue = {}
+heap = []
+    
+make_heap(priority_queue, heap, 20)
